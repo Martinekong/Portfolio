0ef95c5f053a1120914570673ec396cf75f32875
Martinekong <martinekongsrud@outlook.com>
2025-02-25 18:26:11 +0100

    added to-do comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function renderPage() {
+function renderPage() { //call this function something else?
   showNavigationMenu();
   showProjectCards();
   copyEmail();
@@ -23,6 +23,8 @@ function showNavigationMenu() {
       navigation.classList.remove("show");
     });
   });
+
+  // need to add event listener to close the navigation when clicking outside
 }
 
 function showProjectCards() {
@@ -36,10 +38,12 @@ function showProjectCards() {
       icon.classList.toggle("rotate");
     });
   });
+
+  // Need to add event listener so that when one card is open, it closes when another is opened
 }
 
 function copyEmail() {
-    // add copy function to the button with envelope in hero section
+  // add copy function to the button with envelope in hero section
   // add copy function to the .email-copy in the contact section
 }
 
